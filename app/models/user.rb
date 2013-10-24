b2d40bb1882e99a28ffd85b87e1d0c0da847677e
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-24 09:48:05 +0200

    Add Fb authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+
+  ## find_for_facebook_oauth(auth, signed_in_ressource)
+  # Create and/or return the signed in user, with a random password
+  # param auth : facebook informations and credentials
+  # param signed_in_ressource : not used now
+  # return connected user
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+    end
+    user
+  end
+
 end
