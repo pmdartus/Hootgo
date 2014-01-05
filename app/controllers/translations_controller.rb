7cb7cb66085d7550269774516ef2832fd05cb927
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-05 15:39:09 +0100

    Set the callback reader

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -3,7 +3,7 @@ class TranslationsController < ApplicationController
   before_action :set_translation, only: [:update_status]
 
   def update_status
-
+    puts ActiveSupport::JSON.decode(request.body.read)
   end
 
   private
@@ -13,12 +13,5 @@ class TranslationsController < ApplicationController
   # if the current_user is not the owner of the camapaign it return a 404
   def set_translation
     @translation = Translation.find(params[:translation_id])
-  end
-
-  ## camapign_params
-  # Define all the aceptable parameters for a post request
-  def translation_params
-    puts params.to_json
-    params.require(:job).permit(:job, :language, :translations => [])
   end
 end
