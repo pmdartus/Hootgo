1c86c13ce1271d309693def7bb3ee7c0a7e39f2b
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-05 15:12:34 +0100

    Delete xcrf attack verification for notification

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,5 +1,6 @@
 class TranslationsController < ApplicationController
-  before_action :set_translation, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token, :only => [:update_status]
+  before_action :set_translation, only: [:update_status]
 
   def update_status
 
