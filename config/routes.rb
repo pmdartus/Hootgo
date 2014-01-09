6c88531931cac2ba5b28dca2374350902dc278cb
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-09 20:28:42 +0100

    Add comments to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Hootgo::Application.routes.draw do
 
+  # Root path
+  root 'home#index'
+
+  # User management
   devise_for :users
 
+  # Callback oauth authentication
   get '/auth/:provider/callback', to: 'authorizations#create'
   resources :authorizations
 
+  # Campaign management
   get "dashboard", to: "campaigns#index"
   get "order", to: "campaigns#new"
 
-
+  # Post management
   resources :campaigns do
   	resources :translations do
         post "update_status", to: "translations#update_status"
     end
   end
-
-  root 'home#index'
 
 end
