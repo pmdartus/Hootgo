b4c510c6eebe92ee4c1f544e28ea375cbce216a6
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-11 19:50:06 +0100

    Udpdate for linking twitter accoutns

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,40 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      auth = request.env["omniauth.auth"]
-      session['fb_access_token'] = auth.credentials.token
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
-
   def twitter
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    auth = request.env["omniauth.auth"]
+    @authorization = Authorization.find_with_oauth(auth)
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    if @authorization.nil?
+      # If no authorizations found, create a brand new one here
+      @authorization = Authorization.create_with_oauth(auth)
+    end
+
+    if current_user
+      if @authorization.user == current_user
+        # Authiorization already link with this account
+        redirect_to root_url, notice: "Account already linked"
+      else
+        # Authorization is not associated with this user
+        @authorization.user = current_user
+        @authorization.save()
+        redirect_to root_url, notice: "Account successfully linked"
+      end
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      if @authorization.user.present?
+        # The authorization was found but the user is not logged in
+        session[:user_id] = @authorization.user_id
+        sign_in_and_redirect @authorization.user, :event => :authentication
+      else
+        # The user is not logged in and the authorization is not found
+        @user = User.create_with_oauth(auth)
+        @authorization.user = @user
+        @authorization.save()
+        @user.save()
+
+        session[:user_id] = @authorization.user_id
+        sign_in_and_redirect @user, :event => :authentication
+      end
     end
   end
+
 end
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,4 +1,13 @@
 class Authorization < ActiveRecord::Base
-    belongs_to :user
-    validates :provider, :user_id, :uid, presence: true
+  belongs_to :user
+  validates :provider, :uid, presence: true
+
+  def self.find_with_oauth(auth)
+    find_by provider: auth.provider, uid: auth.uid
+  end
+
+  def self.create_with_oauth(auth)
+    create(uid: auth.uid, provider: auth.provider)
+  end
+
 end
