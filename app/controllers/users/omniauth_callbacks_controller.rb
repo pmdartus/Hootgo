bc4328001fa97a2dc51b06f225f33bd2a6f0f4a3
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-12 12:05:39 +0100

    Update for charging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-<<<<<<< HEAD
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-    puts request.env["omniauth.auth"].to_json
-    if @user.persisted?
-      auth = request.env["omniauth.auth"]
-      session['fb_access_token'] = auth.credentials.token
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
 
-  def twitter
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    puts request.env["omniauth.auth"].to_json
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"].provider, current_user)
-=======
   def twitter
     auth = request.env["omniauth.auth"]
     puts auth.to_json
@@ -30,8 +10,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # If no authorizations found, create a brand new one here
       @authorization = Authorization.create_with_oauth(auth)
     end
->>>>>>> master
-
     if current_user
       if @authorization.user == current_user
         # Authiorization already link with this account
